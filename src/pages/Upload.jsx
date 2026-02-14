6685cd974e307d5b7d410751d7e629463d73407c
0xyusufz <gurucoc471@gmail.com>
2026-02-14 16:17:51 +0530

    upload page created

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Container,Box, Input, Button } from "@chakra-ui/react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
 
 const Upload = () => {
   return (
-    <div>
-      <h1>Upload video</h1>
-    </div>
-  )
-}
+    <Container maxW={"container.xl"} h={"100vh"} p={"16"}>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        h={"full"}
+        color={"purple.500"}
+      >
+        <AiOutlineCloudUpload size={"10vmax"}/>
 
-export default Upload
+        <form>
+          <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
+            <Input required type={"file"} 
+            css={{
+              "&::file-selector-button": {
+                border: "none",
+                width: "calc(100% + 36px)",
+                height: "100%",
+                marginLeft: "-18px",
+                color: "purple",
+                backgroundColor: "white",
+                cursor: "pointer",
+              },
+            }} />
+            <Button colorScheme={"purple"} type={"submit"}>Upload</Button>
+          </Box>
+        </form>
+      </Box>
+    </Container>
+  );
+};
+
+export default Upload;
