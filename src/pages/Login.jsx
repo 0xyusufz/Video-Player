d6b55d5811b790b165fce6df956d8cdbde019b6f
0xyusufz <gurucoc471@gmail.com>
2026-02-14 16:56:34 +0530

    sign up page created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Heading ,Box, Input, Button,Text} from "@chakra-ui/react";
+import { Container, Heading, Box, Input, Button, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
@@ -12,22 +12,42 @@ const Login = () => {
           gap={"4"}
           alignItems={"stretch"}
           spacing={"8"}
-          w={["full","96"]}
+          w={["full", "96"]}
           m={"auto"}
           my={"16"}
           justifyContent={"center"}
         >
           <Heading>Welcome Back</Heading>
 
-          <Input placeholder={"enter email"} type={"email"} required focusBorderColor={"purple.500"}/>
-          <Input placeholder={"enter password"} type={"password"} required focusBorderColor={"purple.500"}/>
-          <Button variant={"link"} alignSelf={"flex-end"}><Link to={"/forgetpassword"}>forget password?</Link></Button>
-          <Button  colorScheme={"purple"} type={"submit"}>Log in</Button>
+          <Input
+            placeholder={"enter email"}
+            type={"email"}
+            required
+            focusBorderColor={"purple.500"}
+          />
+          <Input
+            placeholder={"enter password"}
+            type={"password"}
+            required
+            focusBorderColor={"purple.500"}
+          />
+          <Button variant={"link"} alignSelf={"flex-end"}>
+            <Link to={"/forgetpassword"}>forget password?</Link>
+          </Button>
+          <Button colorScheme={"purple"} type={"submit"}>
+            Log in
+          </Button>
 
-
-          <Text textAlign={"end"}>New User?{" "}<Button colorScheme={"purple"} variant={"link"} alignSelf={"center"}><Link to={"/signup"}>Sign Up</Link></Button></Text>
-
-          
+          <Text textAlign={"end"}>
+            New User?{" "}
+            <Button
+              colorScheme={"purple"}
+              variant={"link"}
+              alignSelf={"center"}
+            >
+              <Link to={"/signup"}>Sign Up</Link>
+            </Button>
+          </Text>
         </Box>
       </form>
     </Container>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
+import { Container, Heading, Box, Input, Button, Text, Avatar } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>
-      <h1>sign up</h1>
-    </div>
-  )
-}
+    <Container maxW={"container.xl"} h={"100vh"} p={"16"}>
+      <form>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          gap={"4"}
+          alignItems={"stretch"}
+          spacing={"8"}
+          w={["full", "96"]}
+          m={"auto"}
+          my={"16"}
+          justifyContent={"center"}
+        >
+          <Heading>Video Player</Heading>
+          <Avatar alignSelf={"center"} boxSize={"32"}/>
+          <Input
+            placeholder={"enter name"}
+            type={"text"}
+            required
+            focusBorderColor={"purple.500"}
+          />
+
+          <Input
+            placeholder={"enter email"}
+            type={"email"}
+            required
+            focusBorderColor={"purple.500"}
+          />
+          <Input
+            placeholder={"enter password"}
+            type={"password"}
+            required
+            focusBorderColor={"purple.500"}
+          />
+          <Button colorScheme={"purple"} type={"submit"}>
+            Sign Up
+          </Button>
+
+          <Text textAlign={"end"}>
+            Already Signed Up?{" "}
+            <Button
+              colorScheme={"purple"}
+              variant={"link"}
+              alignSelf={"center"}
+            >
+              <Link to={"/login"}>Log In</Link>
+            </Button>
+          </Text>
+        </Box>
+      </form>
+    </Container>
+  );
+};
 
 export default Signup
