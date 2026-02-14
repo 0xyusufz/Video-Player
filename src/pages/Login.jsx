e76cc19536f98a63566074bbcc76d94ec40658bc
0xyusufz <gurucoc471@gmail.com>
2026-02-14 16:47:00 +0530

    login page created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Container, Heading ,Box, Input, Button,Text} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>login</h1>
-    </div>
-  )
-}
+    <Container maxW={"container.xl"} h={"100vh"} p={"16"}>
+      <form>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          gap={"4"}
+          alignItems={"stretch"}
+          spacing={"8"}
+          w={["full","96"]}
+          m={"auto"}
+          my={"16"}
+          justifyContent={"center"}
+        >
+          <Heading>Welcome Back</Heading>
 
-export default Login
+          <Input placeholder={"enter email"} type={"email"} required focusBorderColor={"purple.500"}/>
+          <Input placeholder={"enter password"} type={"password"} required focusBorderColor={"purple.500"}/>
+          <Button variant={"link"} alignSelf={"flex-end"}><Link to={"/forgetpassword"}>forget password?</Link></Button>
+          <Button  colorScheme={"purple"} type={"submit"}>Log in</Button>
+
+
+          <Text textAlign={"end"}>New User?{" "}<Button colorScheme={"purple"} variant={"link"} alignSelf={"center"}><Link to={"/signup"}>Sign Up</Link></Button></Text>
+
+          
+        </Box>
+      </form>
+    </Container>
+  );
+};
+
+export default Login;
